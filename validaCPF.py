7a29756a16ef08ca5f9c485540b327e2ee8dd373
estefancaique <estefancaique@gmail.com>
2020-07-28 20:43:13 -0300

    Estutando Flask
    Fazendo apllicação funcionar

--- validaCPF.py
+++ validaCPF.py
@@ -1,12 +1,16 @@
-#!/usr/bin/python3
-#import io
-#from itertools import permutation
-from flask import Flask
-cpf_lista ='blacklist.txt'
+from flask import Flask, render_template, request # Importa a biblioteca
 
-cpf_valido = False
-#def validaCPF():
-def validaCPF(entrada = str(input("Digite o CPF: "))):
+app = Flask(__name__) # Inicializa a aplicação
+
+@app.route('/')
+def validaCPF():
+#    return render_template('index.html')
+    digitaCPF = request.args.get('cpf')
+    
+    entrada = '{}.{}.{}-{}'.format(digitaCPF[:3], digitaCPF[3:6], digitaCPF[6:9], digitaCPF[9:])
+    cpf_lista ='blacklist.txt'
+    cpf_valido = False
+    
     with open(cpf_lista ,'r') as c:
         linha = 1
         for cpf in c:
@@ -19,9 +23,14 @@ def validaCPF(entrada = str(input("Digite o CPF: "))):
     c.close()
 
     if cpf_valido:
-        print("Block")
+        return 'Block'
     else:
-        print("Livre") 
+        return 'Livre' 
+    
 
+if __name__ == '__validaCPF__':
+  app.run(debug=True) # Executa a aplicação
+  
+  
+  #validaCPF()
 
-validaCPF()
